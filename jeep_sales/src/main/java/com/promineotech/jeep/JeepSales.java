e86716bce3787d82ec1271ecf0d33e3647d7f576
Eric Moncada <38900942+moirla@users.noreply.github.com>
2022-03-17 13:28:52 -0500

    Add files via upload

---
+++ jeep_sales/src/main/java/com/promineotech/jeep/JeepSales.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.promineotech.jeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author moirl
+ *
+ */
+
+@SpringBootApplication
+public class JeepSales {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+   SpringApplication.run(JeepSales.class, args);
+    
+    
+  }
+
+}
---
+++ jeep_sales/src/main/java/com/promineotech/jeep/entity/JeepModel.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.promineotech.jeep.entity;
+
+/**
+ * @author moirl
+ *
+ */
+public enum JeepModel {
+  WRANGLER, GRAND_CHEROKEE, CHEROKEE, COMPASS, RENEGADE, GLADIATOR, WRANGLER_4XE
+}
